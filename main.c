5d28dd3f8cdc4dff1066fc1d8d82b5816d37e432
Farzin Vatani <farzinvatani@outlook.com>
2024-06-16 17:21:54 +0330

    Draw rectangles as pixel in the sdl window

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "constants.h"
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_render.h>
 
 SDL_Window *window;
 SDL_Renderer *renderer;
@@ -352,13 +354,45 @@ void destroy_sdl() {
   SDL_Quit();
 }
 
-void draw() { SDL_RenderPresent(renderer); }
+unsigned int convert_to_256(double pixel_value) { return pixel_value * 255; }
 
-void process_input() {
+void draw(gsl_matrix *m) {
+  SDL_Rect pixel_rect = {0, 0, COLS * SCALE_FACTOR, ROWS * SCALE_FACTOR};
+
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderFillRect(renderer, &pixel_rect);
+
+  for (int row = 0; row < ROWS; row++) {
+    for (int col = 0; col < COLS; col++) {
+      double pixel_value = gsl_matrix_get(m, row, col);
+
+      if (pixel_value == Light) {
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+      } else if (pixel_value < 1.0) {
+        unsigned int pixel_color = convert_to_256(pixel_value);
+        SDL_SetRenderDrawColor(renderer, pixel_color, pixel_color, pixel_color,
+                               255);
+      } else if (pixel_value == Lamp) {
+        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+      } else {
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+      }
+
+      SDL_Rect pixel_rect = {col * SCALE_FACTOR, row * SCALE_FACTOR,
+                             SCALE_FACTOR, SCALE_FACTOR};
+
+      SDL_RenderFillRect(renderer, &pixel_rect);
+    }
+  }
+
+  SDL_RenderPresent(renderer);
+}
+
+void process_input(gsl_matrix *m) {
   int close = FALSE;
 
   while (!close) {
-    draw();
+    draw(m);
     while (SDL_PollEvent(&event)) {
       switch (event.type) {
 
@@ -401,7 +435,7 @@ int main() {
     return 1;
   };
 
-  process_input();
+  process_input(m3);
 
   gsl_matrix_free(m);
   gsl_matrix_free(m2);
